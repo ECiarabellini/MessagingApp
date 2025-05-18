841e07839e6b8b515fee7b5bfce16b88cb3b14b1
Emily Ciarabellini <emily.ciarabellini@guild.com>
2025-05-18 15:17:02 -0700

    Create Message sequelize model

---
+++ Models/Message.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Message extends Model {}
+
+
+Message.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        fromUser: {
+            type: DataTypes.STRING(254),
+            allowNull: false,
+        },
+        toUser: {
+            type: DataTypes.STRING(254),
+            allowNull: false,
+        },
+        subjectLine: {
+            type: DataTypes.STRING(70),
+            allowNull: false,
+        },
+        bodyText: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'message',
+        tableName: 'messages' // Explicitly set to match the SQL
+    }
+);
+
+
+module.exports = Message;
