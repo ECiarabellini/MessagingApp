985c39adc2ffe62f588884d4282a05637e17a7ed
Emily Ciarabellini <emily.ciarabellini@guild.com>
2025-05-17 22:56:26 -0700

    POST new message to console

--- public/index.js
+++ public/index.js
@@ -1,46 +1,53 @@
+const messageForm = document.getElementById('message-form');
+const senderInput = document.getElementById('sender');
+const recipientInput = document.getElementById('recipient');
+const subjectInput = document.getElementById('subject');
+const textInput = document.getElementById('message-text');
 
 
-function Message(to, from, subject, body) {
-    this.to = to;
-    this.from = from;
-    this.subject = subject;
-    this.body = body;
-}
-
-//use prototype for memory efficiency instead of embedding function within Message object
-Message.prototype.sendConfirm = function () {
-    console.log(`Message sent to ${this.to}`);
-}; 
-
-//const message1 = new Message('Jeremy', 'Emily', 'Big News', 'I got a new job!');
-
-//template literal
-let messageDetails =
-    `Message to: ${message1.to}
-From: ${message1.from}
-Subject: ${message1.subject}
-${message1.body}`;
-
-// console.log(messageDetails);
-// console.log(message1.sendConfirm());
+// Helper function that accepts a `message` object, sends a POST request and returns the result
+const postMessage = (message) =>
+    // Fetch accepts a URL and an options object where you can declare the HTTP method, the request body, and any headers.
+    fetch('/api/messages', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log('Successful POST request:', data);
+        return data;
+    })
+    .catch((error) => {
+        console.error('Error in POST request:', error);
+    });
 
 
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!senderInput || !recipientInput || !subjectInput || !textInput) {
+        alert('Please fill in all fields.');
+        return;
+    };
+    
 
+    //create new message object from input values
+    const newMessage = {
+        from: senderInput.value.trim(),
+        to: recipientInput.value.trim(),
+        subject: subjectInput.value.trim(),
+        text: textInput.value.trim()
+    }
+    
 
+  // Call our `postMessage` method to make a POST request with our `newMessage` object.
+    postMessage(newMessage)
+        .then((data) => {
+            alert('Message sent!');
+            messageForm.reset();
+        })
+        .catch((err) => console.error(err));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = Message;
+});
--- server.js
+++ server.js
@@ -23,6 +23,51 @@ app.get('/new', (req, res) =>
 // Endpoint to return own locally stored data
 app.get('/api', (req, res) => res.json(messages));
 
+// POST request to send a message
+app.post('/api/messages', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to post a message`);
+
+    // Destructuring assignment for the items in req.body
+    const { from, to, subject, text } = req.body;
+
+    // If all the required properties are present
+    if (from && to && subject && text)  {
+      // Variable for the object we will save
+        const newMessage = {
+            from,
+            to,
+            subject,
+            text
+        };
+
+        const response = {
+            status: 'success',
+            body: newMessage,
+        };
+
+        console.log(response);
+        // res.json() returns data including a status message indicating the success of the request along with the newly created message data.
+        res.status(201).json(response);
+        } else {
+        // the purpose of the else statement is to allow a way to throw an error if any of the form data is not present.
+        res.status(500).json('Error in sending message');
+        }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // listen() method is responsible for listening for incoming connections on the specified port 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
